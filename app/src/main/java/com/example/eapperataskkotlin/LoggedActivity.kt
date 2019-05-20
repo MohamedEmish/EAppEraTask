f805b21cdb5b621e64fe4b4e9857a967443552b8
Mohamed Emish <emish52020@gmail.com>
2019-05-20 17:59:42 +0200

    Final

--- app/src/main/java/com/example/eapperataskkotlin/LoggedActivity.kt
+++ app/src/main/java/com/example/eapperataskkotlin/LoggedActivity.kt
@@ -1,17 +1,13 @@
 package com.example.eapperataskkotlin
 
 import android.Manifest
-import android.app.Activity
 import android.app.PendingIntent
-import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
@@ -22,13 +18,12 @@ import com.karumi.dexter.listener.PermissionGrantedResponse
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.single.PermissionListener
 import kotlinx.android.synthetic.main.activity_logged.*
-import java.net.CacheRequest
 
 class
 LoggedActivity : AppCompatActivity() {
 
-    lateinit var client: FusedLocationProviderClient
-    lateinit var locationRequest: LocationRequest
+    private lateinit var client: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
     companion object{
         var instance : LoggedActivity? = null
 
@@ -97,7 +92,7 @@ LoggedActivity : AppCompatActivity() {
 
     private fun getPendingIntent() : PendingIntent?{
         val intent = Intent(this@LoggedActivity,MyLocationService:: class.java)
-        intent.setAction(MyLocationService.ACTION_PROCESS_UPDATED)
+        intent.action = MyLocationService.ACTION_PROCESS_UPDATED
 
         return PendingIntent.getBroadcast(this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT)
     }
@@ -110,45 +105,5 @@ LoggedActivity : AppCompatActivity() {
         locationRequest.smallestDisplacement = 1f
 
 
-    }
-
-    private fun hasPermission(context: Context, activity: Activity): Boolean {
-        if (ContextCompat.checkSelfPermission(
-                context,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            return true
-        } else {
-            requestPermission(context, activity)
-        }
-        return false
-    }
-
-    private fun requestPermission(context: Context, activity: Activity) {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(
-                activity, Manifest.permission.ACCESS_FINE_LOCATION
-            )
-        ) {
-
-            AlertDialog.Builder(context)
-                .setTitle("Permission is needed")
-                .setMessage("Needed to get your location")
-                .setPositiveButton(
-                    "OK"
-                ) { _, _ ->
-                    ActivityCompat.requestPermissions(
-                        activity,
-                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                        100
-                    )
-                }
-                .setNegativeButton(
-                    "Cancel"
-                ) { dialogInterface, _ -> dialogInterface.dismiss() }
-                .create().show()
-        } else {
-            ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 100)
-        }
     }
 }
--- app/src/main/java/com/example/eapperataskkotlin/MyLocationService.kt
+++ app/src/main/java/com/example/eapperataskkotlin/MyLocationService.kt
@@ -1,28 +1,23 @@
 package com.example.eapperataskkotlin
 
-import android.app.Service
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.os.IBinder
 import android.widget.Toast
-import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
-import java.lang.Exception
-import java.lang.StringBuilder
 
 class MyLocationService : BroadcastReceiver() {
     companion object{
-        val ACTION_PROCESS_UPDATED = "com.example.eapperataskkotlin.UPDATE_LOCATION"
+        const val ACTION_PROCESS_UPDATED = "com.example.eapperataskkotlin.UPDATE_LOCATION"
     }
     override fun onReceive(context: Context?, intent: Intent?) {
         if (intent != null){
-            val action = intent!!.action
-            if (action.equals(ACTION_PROCESS_UPDATED)){
+            val action = intent.action
+            if (action == ACTION_PROCESS_UPDATED) {
                 val result = LocationResult.extractResult(intent)
                 if (result != null){
                     val  location = result.lastLocation
-                    var  locationString  = StringBuilder(location.longitude .toString())
+                    val locationString = StringBuilder(location.longitude.toString())
                         .append("\\n")
                         .append(location.latitude.toString())
                         .toString()
